841364dc4855a42842a6af6558fdc2a9984b65bd
David Cruz García <david.cruz.garciaa@gmail.com>
2021-04-02 18:21:59 +0200

    Añadido envio suma acumulada al siguiente proceso

--- main.c
+++ main.c
@@ -11,17 +11,28 @@
 //Declaracion de tipos
 typedef unsigned long Numero;
 
+typedef struct infoProceso{
+	int id;
+	Numero tCalculo;
+	int nDivisores;
+	Numero sumaIntervalo;
+} InfoProceso;
+
+
 
 //Prototipos de funciones
 Numero calculoDivisores(Numero num, Numero iInicio, Numero iFinal);
 
+
+
+/* MAIN */
 int main(int argc, char **argv){
 	if(argc != 2) return 1;
 	
 	int id, nProc;
 	Numero intervalo, num;
 	
-	
+	MPI_Status status;
 	
 	
 	
@@ -35,6 +46,7 @@ int main(int argc, char **argv){
 	//  y enviamos ambos a todos los procesos
 	if(ES_PROCESO_0(id)){
 		num = strtoul(argv[1], NULL, 10);
+		printf("NUMERO A COMPROBAR: %lu EN %d PROCESOS\n\n", num, nProc);
 		intervalo = num / (nProc - 1);
 	}
 	
@@ -44,8 +56,12 @@ int main(int argc, char **argv){
 	
 	//Cálculo
 	Numero sumaIntervalo = 0;
+	Numero sumaRecibida = 0;
+	
 	if(ES_PROCESO_0(id)){
 		//Esperar por los mensajes
+		MPI_Recv(&sumaRecibida, 1, MPI_UNSIGNED_LONG, nProc-1, 5, MPI_COMM_WORLD, &status);
+		printf("%d -> recibido %lu\n", id, sumaRecibida);
 	
 	}else{
 		if(ES_ULTIMO_PROCESO(id)){
@@ -55,12 +71,12 @@ int main(int argc, char **argv){
 		}
 	}
 	
-	Numero suma=0;
-	MPI_Reduce(&sumaIntervalo, &suma, 1, MPI_UNSIGNED_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+	//Numero suma=0;
+	//MPI_Reduce(&sumaIntervalo, &suma, 1, MPI_UNSIGNED_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
 	
 	if(ES_PROCESO_0(id)){
-		printf("Suma total: %lu\n", suma);
-		if(num == suma){
+		printf("Suma total: %lu\n", sumaRecibida);
+		if(num == sumaRecibida){
 			printf("\nEl numero %lu es perfecto\n", num);
 		}else{
 			printf("\nEl numero %lu no es perfecto\n", num);
@@ -78,24 +94,41 @@ int main(int argc, char **argv){
 
 Numero calculoDivisores(Numero num, Numero iInicio, Numero iFinal){
 
-	Numero divisor, sumaIntervalo;
+	int nProc, id;
+	MPI_Comm_rank(MPI_COMM_WORLD, &id);
+	MPI_Comm_size(MPI_COMM_WORLD, &nProc);
 	
+	Numero divisor, sumaIntervalo, sumaAcum;
+	
+	sumaIntervalo = 0;
+	sumaAcum = 0;
+	
+//Coger tInicio
 	for(divisor=iInicio; divisor<=iFinal; divisor++){
 		if(divisor!=num && ES_DIVISOR(divisor, num)){
 			//Enviar el divisor
 			sumaIntervalo += divisor;
 		}
 	}
+//Coger tFin
 	
+	
+	//Espero por la suma acumulada de los anteriores (el primer proceso no espera porque el anterior
+	// es el 0, y por tanto, no va a calcular nada)
+	if(id > 1){
+		MPI_Recv(&sumaAcum, 1, MPI_UNSIGNED_LONG, id-1, 5, MPI_COMM_WORLD, NULL);
+	}
+	
+	sumaAcum += sumaIntervalo;
 	
 	//Enviar a proceso siguiente su resultado sumado al de los procesos anteriores. Si 
 	// es el último proceso, le envía el resultado al 0
-	/*int etiqueta = 5;
+	int etiqueta = 5;
 	if(ES_ULTIMO_PROCESO(id)){
-		MPI_Send(&sumaIntervalo, 1, MPI_UNSIGNED_LONG, 0, etiqueta, MPI_COMM_WORLD);
+		MPI_Send(&sumaAcum, 1, MPI_UNSIGNED_LONG, 0, etiqueta, MPI_COMM_WORLD);
 	}else{	
-		MPI_Send(&sumaIntervalo, 1, MPI_UNSIGNED_LONG, datos.pID+1, etiqueta, MPI_COMM_WORLD);
-	}*/
+		MPI_Send(&sumaAcum, 1, MPI_UNSIGNED_LONG, id+1, etiqueta, MPI_COMM_WORLD);
+	}
 	
 	return sumaIntervalo;
 }
